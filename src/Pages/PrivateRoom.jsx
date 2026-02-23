49478522b171536c9b06307e6789f6186249b253
kanha-beep <kanhashree2223@gmail.com>
2026-02-23 14:27:14 +0530

    bot

--- src/Pages/PrivateRoom.jsx
+++ src/Pages/PrivateRoom.jsx
@@ -271,7 +271,7 @@ export default function PrivateRoom() {
           const blob = new Blob(chunks, { type: "audio/webm" });
 
           const res = await fetch(
-            "http://localhost:3000/api/discussion/audio",
+            `${import.meta.env.VITE_API_URL}/api/audio`,
             {
               method: "POST",
               headers: { "Content-Type": "application/octet-stream" },
